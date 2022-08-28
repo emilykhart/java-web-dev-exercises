fca282c16246092d9dfea941e7199cb150fbee38
Emily Hart <emilykhart726@gmail.com>
2022-08-28 11:45:25 -0500

    exercises

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn5unittesting.main;
 
-public class Car {
+public class
+Car {
 
     private String make;
     private String model;
@@ -47,6 +48,9 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size");
+        }
         this.gasTankLevel = gasTankLevel;
     }
 
@@ -83,6 +87,9 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
